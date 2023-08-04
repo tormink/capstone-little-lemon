243813195170b71b9326fcc21aad63bc1245b179
Lucas Tormin <lucasntormin@gmail.com>
2023-08-04 10:10:27 -0300

    Added styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,33 +1,39 @@
 import PriceCard from './PriceCard';
-import heroImg from './../assets/restauranfood.jpg';
 import './main.css';
+import heroImg from './../assets/restaurantfood_.png';
 import greekSalad from './../assets/greek salad.jpg';
 import bruschetta from './../assets/bruchetta.svg';
 import lemonDessert from './../assets/lemon dessert.jpg';
 
-const mainPadding = { padding: '0 70px' };
+
 const Main = () => (
-    <main style={mainPadding}>
+    <main>
         <section className='hero'>
             <article>
                 <h1>Little Lemon</h1>
                 <h2>Chicago</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <a className="button-primary" href="#">Reserve a table</a>
             </article>
             <aside>
                 <img src={heroImg} alt="" />
             </aside>
         </section>
         <section className='specials'>
-            <PriceCard title='Greek Salad' img={greekSalad} price='12.99'>
-            The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. 
-            </PriceCard>
-            <PriceCard title='Bruschetta' img={bruschetta} price='5.99'>
-            Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil. 
-            </PriceCard>
-            <PriceCard title='Lemon Dessert' img={lemonDessert} price='5.00'>
-            This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.
-            </PriceCard>
+            <h2>Specials</h2>
+            <a className="button-primary" href="#">Online Menu</a>
+            <div className='cards'>
+
+                <PriceCard title='Greek Salad' img={greekSalad} price='12.99'>
+                    The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.
+                </PriceCard>
+                <PriceCard title='Bruschetta' img={bruschetta} price='5.99'>
+                    Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.
+                </PriceCard>
+                <PriceCard title='Lemon Dessert' img={lemonDessert} price='5.00'>
+                    This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.
+                </PriceCard>
+            </div>
         </section>
     </main>
 )
--- src/components/PriceCard.js
+++ src/components/PriceCard.js
@@ -1,11 +1,14 @@
-import {ReactComponent as DeliveryIcon} from '../assets/delivery.svg';
-const PriceCard = ({img, price, title, children}) =>(
-    <article>
+import { ReactComponent as DeliveryIcon } from '../assets/delivery.svg';
+import './pricecard.css';
+const PriceCard = ({ img, price, title, children }) => (
+    <article className='card'>
         <img src={img} alt="dish" />
-        <h3>{title}</h3>
-        <span className='card-price'>$ {price}</span>
-        <p>{children}</p>
-        <a href="#">Order a delivery <DeliveryIcon/></a>
+        <div>
+            <h3>{title}</h3>
+            <span className='card-price'>$ {price}</span>
+            <p>{children}</p>
+            <a href="#">Order a delivery <DeliveryIcon /></a>
+        </div>
     </article>
 
 );
