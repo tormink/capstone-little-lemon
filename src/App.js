77158fbf4cb65163f3e9b87a52273c65bfa88907
Lucas Tormin <lucasntormin@gmail.com>
2023-08-07 10:50:42 -0300

    Refactoring for adding new pages part 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Main from './components/Main';
+import HomePage from './HomePage';
 
 function App() {
   return (
     <>
-      <Header/>
-      <Main/>
-      <Footer/>
+      <HomePage/>
     </>
   );
 }
