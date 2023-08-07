6ddfc15871e09ad399448b62a5435946cd0d3c79
Lucas Tormin <lucasntormin@gmail.com>
2023-08-07 10:50:21 -0300

    Refactor to add multiple pages

---
+++ src/HomePage.js
@@ -0,0 +1,13 @@
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+const HomePage = () => (
+    <>
+        <Header />
+        <Main />
+        <Footer />
+    </>
+);
+
+export default HomePage;
